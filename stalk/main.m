897eb0574edc1ca829ed1d5909c58c577d26240f
Coding <coding@macbook-pro.local>
2016-11-10 22:09:06 +0800

    Initial Commit

---
+++ stalk/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  stalk
+//
+//  Created by Coding on 10/11/2016.
+//  Copyright © 2016 Coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
